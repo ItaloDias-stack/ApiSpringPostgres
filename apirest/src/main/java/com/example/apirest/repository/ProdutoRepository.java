2fdb033d9c455d8188aa7135a3d7e4c3693a4b40
italo freitas <italo-2001@hotmail.com>
2020-02-27 22:38:43 -0300

    Criação do CRUD de Produtos

--- apirest/src/main/java/com/example/apirest/repository/ProdutoRepository.java
+++ apirest/src/main/java/com/example/apirest/repository/ProdutoRepository.java
@@ -1,9 +1,21 @@
 package com.example.apirest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.apirest.models.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-
+	
+	Produto findById(long id);
+	
+	@Query(value = "select * from produto where nome = ?1", nativeQuery = true)
+	Produto findByNome(String nome);
+	
+	@Query(value = " delete from produto where id_produto = ?1", nativeQuery = true)
+	Produto remove(long id);
+	
+	@Query(value = "update produto set nome = ?1, preco = ?2 where id_produto = ?3", nativeQuery = true)
+	Produto edita(String nome,double preco,long id);
+	
 }
--- apirest/src/main/java/com/example/apirest/resources/Produtos.java
+++ apirest/src/main/java/com/example/apirest/resources/Produtos.java
@@ -1,8 +1,19 @@
 package com.example.apirest.resources;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +23,58 @@ import com.example.apirest.repository.ProdutoRepository;
 @RestController
 @RequestMapping(value = "/produto")
 public class Produtos {
-	
+
 	@Autowired
 	ProdutoRepository produtoRepository;
-	
-	@RequestMapping(value = "/getProdutos")
-	public List<Produto> getProdutos(){
+
+	@GetMapping(value = "/getProdutos")
+	public List<Produto> getProdutos() {
 		return produtoRepository.findAll();
 	}
-	
+
+	@GetMapping(value = "/getProduto/{id}")
+	public Produto getProduto(@PathVariable(value = "id") long id) {
+		return produtoRepository.findById(id);
+	}
+
+	public boolean verificaProduto(Produto produto) {
+		if (produtoRepository.findByNome(produto.getNome()) == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@PostMapping("/saveProduto")
+	public ResponseEntity<Produto> saveProduto(@RequestBody Produto produto) {
+		if (verificaProduto(produto)) {
+			produtoRepository.save(produto);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
+
+	@GetMapping("/deleteProduto/{id}")
+	public ResponseEntity<Produto> deleteProduto(@PathVariable(value = "id") long id) {
+		Produto produto = produtoRepository.findById(id);
+		produtoRepository.delete(produto);
+		return new ResponseEntity<>(HttpStatus.OK);
+
+	}
+
+	@PutMapping("/updateProduto/{id}")
+	public ResponseEntity<Produto> updateProduto(@PathVariable(value = "id") long id, @RequestBody Produto p) {
+
+		Produto produto = produtoRepository.findById(id);
+
+		produto.setLoja(p.getLoja());
+		produto.setNome(p.getNome());
+		produto.setPreco(p.getPreco());
+
+		Produto updatedProduto = produtoRepository.save(produto);
+
+		return ResponseEntity.ok(updatedProduto);
+
+	}
+
 }
